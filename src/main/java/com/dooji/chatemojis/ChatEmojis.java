bdc8c50c7277b6bb59f522dce7bb790c5359c0de
dooji2 <doojisbasement@gmail.com>
2026-01-01 22:12:42 +0200

    bump ver

--- src/main/java/com/dooji/chatemojis/ChatEmojis.java
+++ src/main/java/com/dooji/chatemojis/ChatEmojis.java
@@ -9,7 +9,7 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ChatEmojis.MODID, version = ChatEmojis.VERSION)
 public class ChatEmojis {
     public static final String MODID = "chatemojis";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @SidedProxy(clientSide = "com.dooji.chatemojis.client.ClientProxy", serverSide = "com.dooji.chatemojis.CommonProxy")
     public static CommonProxy proxy;
